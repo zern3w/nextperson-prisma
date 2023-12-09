619dfa8a4eb2eadee324ad57318f81ebe9d2e436
gocallum <callumbir@gmail.com>
2023-12-09 23:06:04 +1100

    added the front-end page.tsx which is migrated from person-next app and created a new lib which has the person. Also forced the page to use client so that it's not server-side rendering by default in app router. also had to change the layout for it to work with app router. and had to change from MuiAlert to Alert for it to work in Next 14

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,240 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
 
-export default function Home() {
+import React, { useEffect, useState } from 'react';
+import Layout from '../app/layout'; 
+
+import {
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Button,
+  TextField,
+  DialogActions,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Snackbar,
+} from '@mui/material';
+
+import Alert from '@mui/material/Alert';
+import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
+import { AlertColor } from '@mui/material/Alert';
+
+import { Person } from '../app/lib/person';
+
+export default function IndexPage() {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [open, setOpen] = useState(false);
+  const [currentPerson, setCurrentPerson] = useState<Person | null>(null);
+  const [editMode, setEditMode] = useState(false);
+
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+
+const [snackbarSeverity, setSnackbarSeverity] = useState<AlertColor>('success'); // Set a default value
+
+  useEffect(() => {
+    fetchPeople();
+  }, []);
+
+  const fetchPeople = async () => {
+    try {
+      const response = await fetch('/api/people');
+      if (response.ok) {
+        const data = await response.json();
+        setPeople(data);
+      } else {
+        console.error('Error fetching people data.');
+      }
+    } catch (error) {
+      console.error('Error fetching people data:', error);
+    }
+  };
+
+  const handleOpen = (person: Person | null) => {
+    setCurrentPerson(person);
+    if (person) setEditMode(true);
+    else setEditMode(false);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setCurrentPerson(null);
+  };
+
+  const handleDelete = async (id: number) => {
+    try {
+      const response = await fetch(`/api/people/${id}`, {
+        method: 'DELETE',
+      });
+      if (response.ok) {
+        setPeople(prevPeople => prevPeople.filter(person => person.id !== id));
+        setSnackbarMessage('Record deleted successfully!');
+        setSnackbarSeverity('success');
+        setSnackbarOpen(true);
+      } else {
+        setSnackbarMessage('Error deleting the record.');
+        setSnackbarSeverity('error');
+        setSnackbarOpen(true);
+      }
+    } catch (error) {
+      console.error('Error deleting the person:', error);
+      setSnackbarMessage('Error deleting the record.');
+      setSnackbarSeverity('error');
+      setSnackbarOpen(true);
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      if (editMode && currentPerson) {
+        const response = await fetch(`/api/people/${currentPerson.id}`, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(currentPerson),
+        });
+        if (response.ok) {
+          const updatedPerson: Person = await response.json();
+          setPeople(prevPeople =>
+            prevPeople.map(person => (person.id === updatedPerson.id ? updatedPerson : person))
+          );
+          setSnackbarMessage('Record updated successfully!');
+          setSnackbarSeverity('success');
+          setSnackbarOpen(true);
+        } else {
+          setSnackbarMessage('Error updating the record.');
+          setSnackbarSeverity('error');
+          setSnackbarOpen(true);
+        }
+      } else {
+        const newPerson = {
+          firstname: currentPerson!.firstname,
+          lastname: currentPerson!.lastname,
+          phone: currentPerson!.phone,
+        };
+        const response = await fetch('/api/people', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(newPerson),
+        });
+        if (response.ok) {
+          const createdPerson: Person = await response.json();
+          setPeople(prevPeople => [...prevPeople, createdPerson]);
+          setSnackbarMessage('Record added successfully!');
+          setSnackbarSeverity('success');
+          setSnackbarOpen(true);
+        } else {
+          setSnackbarMessage('Error adding the record.');
+          setSnackbarSeverity('error');
+          setSnackbarOpen(true);
+        }
+      }
+    } catch (error) {
+      console.error('Error updating/adding the person:', error);
+      setSnackbarMessage('Error updating/adding the record.');
+      setSnackbarSeverity('error');
+      setSnackbarOpen(true);
+    }
+    handleClose();
+  };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Layout>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Button variant="contained" onClick={() => handleOpen(null)}>Add New Person</Button>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Phone</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {people.map(person => (
+              <TableRow key={person.id}>
+                <TableCell>{person.firstname}</TableCell>
+                <TableCell>{person.lastname}</TableCell>
+                <TableCell>{person.phone}</TableCell>
+                <TableCell>
+                  <Button onClick={() => handleOpen(person)}>Edit</Button>
+                  <Button onClick={() => handleDelete(person.id)}>Delete</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{editMode ? 'Edit Person' : 'Add Person'}</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="First Name"
+            fullWidth
+            value={currentPerson?.firstname || ''}
+            onChange={e =>
+              setCurrentPerson(prev => ({ ...prev!, firstname: e.target.value }))
+            }
+          />
+          <TextField
+            margin="dense"
+            label="Last Name"
+            fullWidth
+            value={currentPerson?.lastname || ''}
+            onChange={e =>
+              setCurrentPerson(prev => ({ ...prev!, lastname: e.target.value }))
+            }
+          />
+          <TextField
+            margin="dense"
+            label="Phone"
+            fullWidth
+            value={currentPerson?.phone || ''}
+            onChange={e =>
+              setCurrentPerson(prev => ({ ...prev!, phone: e.target.value }))
+            }
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleSubmit} color="primary">
+            {editMode ? 'Update' : 'Add'}
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <Alert
+          elevation={6}
+          variant="filled"
+          severity={snackbarSeverity}
+          icon={<CheckRoundedIcon fontSize="inherit" />}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          {snackbarMessage}
+        </Alert>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </Snackbar>
+    </Container>
+    </Layout>
+  );
 }
