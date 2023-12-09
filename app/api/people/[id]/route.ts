b1058d7e45c1f842227a809c7fae41a68ad469d8
gocallum <callumbir@gmail.com>
2023-12-09 22:14:38 +1100

    added the dynamic route [id

---
+++ app/api/people/[id]/route.ts
@@ -0,0 +1,102 @@
+//create a get method that will take id from the context , search for the id from prisma
+//if found return the data record
+
+import { PrismaClient } from '@prisma/client';
+
+export async function GET(request: Request, context: any) {
+  const { id } = context.params;
+
+  const prisma = new PrismaClient();
+
+  const person = await prisma.person.findUnique({
+    where: {
+      id: parseInt(id),
+    }
+  })
+  if (!person) {
+    return new Response('Not found', {
+      status: 404,
+    })
+  }
+  return new Response(JSON.stringify(person), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+
+}
+
+export async function PUT(request: Request, context: any) {
+  const { id } = context.params;
+
+  try {
+    const prisma = new PrismaClient();
+
+    const body = await request.json();
+    const { firstname, lastname, phone } = body;
+
+    if (!firstname || !lastname || !phone) {
+      return new Response('Missing required fields', {
+        status: 400,
+      });
+    }
+
+    const updatedPerson = await prisma.person.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        firstname,
+        lastname,
+        phone,
+        // You can add other fields to update here
+      },
+    });
+
+    if (!updatedPerson) {
+      return new Response('Person not found', {
+        status: 404,
+      });
+    }
+
+    return new Response(JSON.stringify(updatedPerson), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    return new Response('Error', {
+      status: 500,
+    });
+  }
+}
+export async function DELETE(request: Request, context: any) {
+  const { id } = context.params;
+
+  try {
+      const prisma = new PrismaClient();
+
+      const deletedPerson = await prisma.person.delete({
+          where: {
+              id: parseInt(id),
+          },
+      });
+
+      if (!deletedPerson) {
+          return new Response('Person not found', {
+              status: 404,
+          });
+      }
+
+      return new Response('Person deleted successfully', {
+          status: 200,
+      });
+  } catch (error) {
+      return new Response('Error', {
+          status: 500,
+      });
+  }
+}
